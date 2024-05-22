ea44a08739e3c3bddcc6bfee1877d7f02c2c764c
KauaFSB <kauaflp234@gmail.com>
2024-05-21 22:21:08 -0300

    Criado o primeiro botão

--- calculadora/App.js
+++ calculadora/App.js
@@ -1,35 +1,19 @@
-import { Text, SafeAreaView, StyleSheet } from 'react-native';
+import { Text, SafeAreaView, StyleSheet } from "react-native";
+import Botao from "./src/components/Botao10";
 
-// You can import supported modules from npm
-import { Card } from 'react-native-paper';
-
-// or any files within the Snack
-import AssetExample from './components/AssetExample';
-
-export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.paragraph}>
-        Change code in the editor and watch it change on your phone! Save to get a shareable url.
-      </Text>
-      <Card>
-        <AssetExample />
-      </Card>
-    </SafeAreaView>
-  );
+export default props => {
+    return(
+        <SafeAreaView style={estilos.container}>
+          <Botao />
+        </SafeAreaView>
+    )
 }
 
-const styles = StyleSheet.create({
+const estilos = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
-    padding: 8,
-  },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-});
+    backgroundColor: "#f3f3f3",
+    justifyContent: "center",
+    alignItems: "center"
+  }
+})
